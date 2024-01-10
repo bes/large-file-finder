d0d5bdd8af824464524405e72981d8399331dda6
Erik Zivkovic <erik@zivkovic.se>
2024-01-10 19:03:16 +0100

    Add new -i --ignore flag to be able to filter ignored files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-extern crate clap;
-extern crate shellexpand;
-extern crate rayon;
-
+use clap::{App, Arg};
+use ignore::WalkBuilder;
 use rayon::prelude::*;
-use clap::{Arg, App};
+use std::borrow::{BorrowMut, Cow};
+use std::cmp::max;
 use std::error::Error;
-use std::fs::read_dir;
-use std::path::Path;
-use std::cmp::{max, Ordering};
-use std::borrow::{Cow, BorrowMut};
-use std::sync::{Arc, Mutex};
 use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let matches = App::new("Large file finder")
-        .version("1.0")
+        .version("0.2.0")
         .arg(
             Arg::with_name("directory")
                 .value_name("DIRECTORY")
@@ -32,24 +27,37 @@ fn main() {
                 .help("Show files and dirs larger than this percentage of the largest file")
                 .long_help("Show all files and directories that are larger than X% of the largest found file."),
         )
+        .arg(
+            Arg::with_name("ignore")
+                .env("IGNORE")
+                .long("ignore")
+                .short("i")
+                .takes_value(true)
+                .default_value("false")
+                .help("Respect ignore files")
+                .long_help("Respects ignore files when true, counts all files when false (default)."),
+        )
         .get_matches();
 
     let path_str = match matches.value_of("directory") {
         None => {
             println!("Error: must provide a directory");
             return;
-        },
+        }
         Some(path_str) => path_str,
     };
 
     let percent = match matches.value_of("percent") {
-        Some(percent_str) => {
-            match f64::from_str(percent_str) {
-                Ok(pct) => pct,
-                Err(e) => panic!(),
-            }
+        Some(percent_str) => match f64::from_str(percent_str) {
+            Ok(pct) => pct,
+            Err(_) => panic!(),
         },
         None => panic!(),
+    };
+
+    let ignore = match matches.value_of("ignore") {
+        Some(ignore) => bool::from_str(ignore).unwrap_or_else(|_| false),
+        None => false,
     };
 
     let expanded_path_str = match shellexpand::full(path_str) {
@@ -58,9 +66,9 @@ fn main() {
             panic!("Error: {}", e);
         }
     };
-    let mut base_dir = Dir::new( &expanded_path_str);
+    let mut base_dir = Dir::new(&expanded_path_str);
 
-    match find_all_files_and_directories(&mut base_dir) {
+    match find_all_files_and_directories(&mut base_dir, ignore) {
         Ok(_) => (),
         Err(e) => {
             panic!("Error: {}", e);
@@ -76,18 +84,22 @@ fn main() {
     println!("Largest child: {}", bytes_to_nice(largest_child));
 }
 
-fn find_all_files_and_directories(dir: &mut Dir) -> Result<(), Box<dyn Error>> {
-    let path = Path::new(&dir.path);
-    let read_dir = read_dir(path)?;
+fn find_all_files_and_directories(dir: &mut Dir, ignore: bool) -> Result<(), Box<dyn Error>> {
+    // let path = Path::new(&dir.path);
+    // let read_dir = read_dir(path)?;
 
-    dir.children = read_dir
+    println!("Ignore {}", ignore);
+
+    dir.children =
+        // read_dir
+        WalkBuilder::new(&dir.path).standard_filters(ignore).build()
         .map(|f| Arc::new(f))
         .par_bridge()
         .fold(
             || Arc::new(Mutex::new(Vec::<FsItem>::new())),
-            |mut children, entry_result| {
+            |children, entry_result| {
                 let entry = match entry_result.as_ref() {
-                    Err(e) => panic!("No result"),
+                    Err(_) => return children,
                     Ok(de) => de,
                 };
 
@@ -104,39 +116,32 @@ fn find_all_files_and_directories(dir: &mut Dir) -> Result<(), Box<dyn Error>> {
                         return children;
                     }
                 };
-                if metadata.is_dir() {
-                    let mut new_dir = Dir::new(entry_path);
-                    find_all_files_and_directories(&mut new_dir).unwrap();
-                    children.lock().unwrap().push(FsItem::Dir(new_dir));
-                } else {
-                    let new_file = File::new(
-                        metadata.len(),
-                        entry_path,
-                    );
+                if !metadata.is_dir() {
+                    let new_file = File::new(metadata.len(), entry_path);
                     children.lock().unwrap().push(FsItem::File(new_file));
                 }
                 children
-            })
+            },
+        )
         .reduce(
             || Arc::new(Mutex::new(Vec::<FsItem>::new())),
             |acc, val| {
                 acc.lock().unwrap().append(val.lock().unwrap().borrow_mut());
                 acc
-            });
+            },
+        );
 
     Ok(())
 }
 
 enum FsItem {
     File(File),
-    Dir(Dir),
 }
 
 impl FsItem {
     fn as_file_data(&mut self) -> &mut dyn FileData {
         match self {
             FsItem::File(file) => file,
-            FsItem::Dir(dir) => dir,
         }
     }
 }
@@ -158,7 +163,11 @@ struct Dir {
 
 impl Dir {
     fn new(path: &str) -> Dir {
-        Dir { path: path.to_owned(), children: Arc::new(Mutex::new(Vec::new())), size: None }
+        Dir {
+            path: path.to_owned(),
+            children: Arc::new(Mutex::new(Vec::new())),
+            size: None,
+        }
     }
 }
 
@@ -166,7 +175,7 @@ impl FileData for Dir {
     fn calc_size(&mut self) {
         let mut total_size: u64 = 0;
         for child in self.children.lock().unwrap().iter_mut() {
-            let mut fd = child.as_file_data();
+            let fd = child.as_file_data();
             fd.calc_size();
             total_size += fd.size();
         }
@@ -174,10 +183,7 @@ impl FileData for Dir {
     }
 
     fn size(&self) -> u64 {
-        match self.size {
-            None => 0,
-            Some(s) => s,
-        }
+        self.size.unwrap_or_else(|| 0)
     }
 
     fn largest_child(&self) -> u64 {
@@ -199,7 +205,9 @@ impl FileData for Dir {
         let sz = self.size();
         if sz >= cutoff {
             println!("{0: <8} d {1}", bytes_to_nice(sz), self.path);
-            self.children.lock().unwrap()
+            self.children
+                .lock()
+                .unwrap()
                 .iter_mut()
                 .map(|fsi| fsi.as_file_data())
                 .for_each(|f| f.print(cutoff));
@@ -215,7 +223,10 @@ struct File {
 
 impl File {
     fn new(size: u64, path: &str) -> File {
-        File { size, path: path.to_owned() }
+        File {
+            size,
+            path: path.to_owned(),
+        }
     }
 }
 
